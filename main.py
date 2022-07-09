9002aa10fbc831920fc444a17f9e631dd2b7bffa
Vinay Kumar <vishalmessi550@gmail.com>
2022-07-09 15:48:58 +0530

    grp call exist

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ async def skip(client, message):
 @app.on_message(filters.command(["j", "join", "j@Musix_bot2", "join@Musix_bot2"]))
 async def join(client, message):       
     try:
+        await print(app_call.get_call(message.chat.id))
         src_url = yt_dl.src_find(link[0])        
         await app_call.join_group_call(message.chat.id, AudioPiped(src_url), stream_type=StreamType().pulse_stream)
         del link[0]
